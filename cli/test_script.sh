ffb1bd62967836f2aac66043c4e6eff930a0bfe0
Cuong Nguyen <ancuongnguyen07@gmail.com>
2023-12-31 16:10:29 +0200

    [cli] fix name mismatching in test_script

--- cli/test_script.sh
+++ cli/test_script.sh
@@ -3,5 +3,5 @@
 BUILD_DIR=./build
 BLURHASH_CLI=${BUILD_DIR}/blurhash_cli
 
-HASH=$(${BLURHASH} encode --filepath ../imgs/salad.png --xcomp 4 --ycomp 3)
+HASH=$(${BLURHASH_CLI} encode --filepath ../imgs/salad.png --xcomp 4 --ycomp 3)
 ${BLURHASH_CLI} decode --hash "${HASH}" --width 1024 --height 800 --punch 3 --dest ${BUILD_DIR}/output.png
